dec101a5f84fdf6f4de3500d71fe126baef9d502
MMs-gitH <l12202306l@gmail.com>
2025-05-11 11:16:39 +0800

    Update __init__.py

--- Fittrack/__init__.py
+++ Fittrack/__init__.py
@@ -1,26 +1,32 @@
 from flask import Flask
+from flask_wtf import CSRFProtect
 from .models import db
-from .routes import register_routes
+
+csrf = CSRFProtect()
 
 def create_app():
     app = Flask(__name__)
     app.secret_key = 'group10'
 
-    # Cookie/session configuration for dev environment
+    # Session & CSRF config
     app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
     app.config['SESSION_COOKIE_SECURE'] = False
-    # Set up the database URI
+    app.config['WTF_CSRF_SECRET_KEY'] = 'another-strong-secret'
+
+    # Database config
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    # Initialize SQLAlchemy
+    # Init extensions
     db.init_app(app)
+    csrf.init_app(app)
 
-    # Create a table in the application context
+    # Register all route blueprints
+    from .routes import register_blueprints
+    register_blueprints(app)
+
     with app.app_context():
         db.create_all()
 
-    # routes
-    register_routes(app)
-
     return app
+
