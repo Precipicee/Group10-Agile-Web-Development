012cfb3f2a5a3aa6c66808c6b51aff166317ecde
MMs-gitH <l12202306l@gmail.com>
2025-05-13 22:03:35 +0800

    Update forms.py

--- Fittrack/forms.py
+++ Fittrack/forms.py
@@ -1,9 +1,22 @@
 from flask_wtf import FlaskForm
 from wtforms import (
     StringField, PasswordField, SubmitField, BooleanField,
-    DateField, RadioField, IntegerField, FloatField, HiddenField
+    DateField, RadioField, IntegerField, FloatField, HiddenField, ValidationError
 )
 from wtforms.validators import DataRequired, Email, EqualTo, Length, Optional
+import re
+
+def validate_strong_password(form, field):
+    password = field.data
+    if len(password) < 6:
+        raise ValidationError("Password must be at least 6 characters long.")
+    if not re.search(r'[A-Z]', password):
+        raise ValidationError("Password must contain at least one uppercase letter.")
+    if not re.search(r'[a-z]', password):
+        raise ValidationError("Password must contain at least one lowercase letter.")
+    if not re.search(r'[0-9]', password):
+        raise ValidationError("Password must contain at least one number.")
+
 
 class SigninForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=3)])
@@ -12,11 +25,18 @@ class SigninForm(FlaskForm):
     submit = SubmitField('Sign In')
 
 class SignupForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=3)])
+    username = StringField('Username', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
-    confirm = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Create Account')
+    password = PasswordField('Password', validators=[
+        DataRequired(),
+        validate_strong_password
+    ])
+    confirm = PasswordField('Confirm Password', validators=[
+        DataRequired(),
+        EqualTo('password', message='Passwords must match')
+    ])
+    submit = SubmitField('Sign Up')
+
 
 class BasicInfoForm(FlaskForm):
     avatar = HiddenField('Avatar', validators=[DataRequired()])
@@ -40,3 +60,17 @@ class EditProfileForm(FlaskForm):
     target_exercise_time_per_week = IntegerField('Exercise per Week', validators=[Optional()])
     target_exercise_timeframe_days = IntegerField('Exercise Plan Duration', validators=[Optional()])
     submit = SubmitField('Save Changes')
+
+
+class ChangePasswordForm(FlaskForm):
+    current_password = PasswordField('Current Password', validators=[DataRequired()])
+    new_password = PasswordField('New Password', validators=[
+        DataRequired(),
+        validate_strong_password
+    ])
+    confirm_new_password = PasswordField('Confirm New Password', validators=[
+        DataRequired(),
+        EqualTo('new_password', message='Passwords must match')
+    ])
+    submit = SubmitField('Change Password')
+
