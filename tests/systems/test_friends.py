6d95dd32e09ebdb328eb7288ff12edd30786ad84
MMs-gitH <l12202306l@gmail.com>
2025-05-17 00:03:55 +0800

    Update test_friends.py

--- tests/systems/test_friends.py
+++ tests/systems/test_friends.py
@@ -2,7 +2,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
-from tests.system.test_base import BaseSystemTestCase
+from tests.systems.test_base import BaseSystemTestCase
 
 
 class FriendTest(BaseSystemTestCase):
