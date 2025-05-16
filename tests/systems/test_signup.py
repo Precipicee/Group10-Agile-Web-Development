486f50280979a04390d9f5b896e006ea7bde3afa
MMs-gitH <l12202306l@gmail.com>
2025-05-17 00:05:39 +0800

    Update test_signup.py

--- tests/systems/test_signup.py
+++ tests/systems/test_signup.py
@@ -1,4 +1,4 @@
-from tests.system.test_base import BaseSystemTestCase
+from tests.systems.test_base import BaseSystemTestCase
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
