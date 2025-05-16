cf2f5192cf1c27fa737383059598710cf701c086
MMs-gitH <l12202306l@gmail.com>
2025-05-16 19:53:29 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Check if venv exists, if not create it.
-if [ ! -d "application-env" ]; then
+if [ ! -d "venv" ]; then
     echo "Creating virtual environment..."
-    python3 -m venv application-env
+    python3 -m venv venv
 fi
 
 # Activate the venv.
-source application-env/bin/activate
+source venv/bin/activate
 
-# Install dependencies if needed
+# Install dependencies if not installed
 pip install -r requirements.txt
 
 # Run the Flask app
