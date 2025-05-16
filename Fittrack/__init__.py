108638186a33f42e89342f93bf69f968bbea89b1
ZacAster <24648002@student.uwa.edu.au>
2025-05-16 22:33:13 +0800

    update init

--- Fittrack/__init__.py
+++ Fittrack/__init__.py
@@ -18,7 +18,7 @@ def create_app(config_class=Config):
     csrf.init_app(app)
     login_manager.init_app(app)
     login_manager.login_view = 'auth_bp.signin'
-    migrate = Migrate(app, db)
+    Migrate(app, db)
 
     from .routes import register_blueprints
     register_blueprints(app)   
